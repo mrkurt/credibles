aac9b81fac537b1bc1c1a31bcbfcfee7c86b9f12
mrkurt <kurt@mubble.net>
2011-02-17 15:14:47 -0600

    Configuring things for heroku

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,1 +1,8 @@
-Resque.redis.namespace = "resque:credibles:test" if Rails.env == 'test'
+if Rails.env == 'test'
+  Resque.redis.namespace = "resque:credibles:test" if Rails.env == 'test'
+end
+
+if Rails.env == 'production'
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
