e6ccbdf1881a6dc6abf6e345cada6b9bb1ef4fa4
mrkurt <kurt@mubble.net>
2011-02-17 15:42:58 -0600

    Making yaml stuff safer

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-require 'yaml'
-YAML::ENGINE.yamler= 'syck'
+if Module.const_defined?(:YAML) && YAML.const_defined?(:ENGINE)
+  require 'yaml'
+  YAML::ENGINE.yamler= 'syck'
+end
 
 # Initialize the rails application
 Credibles::Application.initialize!
